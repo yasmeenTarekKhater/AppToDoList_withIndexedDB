67d2e8c2b8684a96e374c846d5d1353b8287db17
yasmeen <yasmeentarek112001@gmail.com>
2024-01-19 12:28:42 +0200

    handle deleteing task , Allow notification button,show notification to user when task time comes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 //fetch addButton
 const addTaskBtn = document.getElementById("add-btn");
+const notifyButton = document.querySelector(".allow_nofication");
 
 //create our Indb
 const openRequest = indexedDB.open("todoApp", 2);
@@ -10,11 +11,11 @@ openRequest.onupgradeneeded = function (event) {
     keyPath: "id",
     autoIncrement: true,
   });
-  objectStore.createIndex("notifiedIndex", "notified", { unique: false });
+  objectStore.createIndex('notifiedIndex', "notified", { unique: false });
 };
-
+let db;
 openRequest.onsuccess = function (event) {
-  const db = event.target.result;
+  db = event.target.result;
   // console.log(db);
   //add task to indexed DB
   function addTask(task) {
@@ -22,13 +23,64 @@ openRequest.onsuccess = function (event) {
     const objectStore = transaction.objectStore("tasks");
     const request = objectStore.add(task);
 
-    request.onsuccess = function () {
-      console.log("task added sucessfully");
+    request.onsuccess = function (event) {
+      const generatedId = event.target.result;
+      task.id= generatedId;
+      console.log("task added sucessfully",generatedId);
     };
     request.onerror = function () {
       console.log("error adding task");
     };
   }
+  //delete task from indexed DB
+  function deleteTask(taskId) {
+    const transaction = db.transaction(["tasks"], "readwrite");
+    const objectStore = transaction.objectStore("tasks");
+    const request = objectStore.delete(taskId);
+
+    request.onsuccess = function () {
+      console.log("task deleted sucessfuly");
+    };
+    request.onerror = function () {
+      console.log("error deleteing this task");
+    };
+  }
+  //fetch tasks from IndexedDB and show on ul
+  function getAllTasks() {
+    const transaction = db.transaction(["tasks"], "readonly");
+    const objectStore = transaction.objectStore("tasks");
+    const request = objectStore.getAll();
+
+    request.onsuccess = function () {
+      const tasks = request.result;
+      const taskList = document.getElementById("my-tasks");
+      taskList.innerHTML = "";
+
+      tasks.forEach((task) => {
+        const li = document.createElement("li");
+        li.textContent = task.name;
+
+        if (task.notified) {
+          li.classList.add("completed");
+        }
+
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("deleteBtn");
+        deleteButton.textContent = "X";
+        deleteButton.addEventListener("click", function () {
+          deleteTask(task.id);
+          taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteButton);
+        taskList.appendChild(li);
+      });
+    };
+    request.onerror = function () {
+      console.log("error getting tasks from Indexed DB");
+    };
+  }
+  //get inputs value and add to ul & push object to addTask function
   addTaskBtn.addEventListener("click", function () {
     const taskName = document.getElementById("task_title").value;
     const taskHours = document.getElementById("task_hours").value;
@@ -48,56 +100,64 @@ openRequest.onsuccess = function (event) {
     li.textContent = taskName;
 
     const deleteButton = document.createElement("button");
+    deleteButton.classList.add("deleteBtn");
     deleteButton.textContent = "X";
+
+    deleteButton.addEventListener("click", function () {
+      deleteTask(newtask.id);
+      taskList.removeChild(li);
+    });
 
     li.appendChild(deleteButton);
     taskList.appendChild(li);
-});
+  });
+  function checkAndNotifyTasks() {
+      const transaction = db.transaction(['tasks'], 'readonly');
+      const objectStore = transaction.objectStore('tasks');
+      const getAllRequest = objectStore.getAll();
+      // console.log(getAllRequest)
+      getAllRequest.onsuccess=function(){
+        const tasks=getAllRequest.result;
+        tasks.forEach(task=>{
+          if(!task.notified && new Date(task.date)<= new Date()){
+            notifyUser(task.name);
+            task.notified=true;
+            const updateTransaction = db.transaction(['tasks'], 'readwrite');
+            const updateStore = updateTransaction.objectStore('tasks');
+            const updateRequest = updateStore.put(task);
+            getAllTasks();//call this function again to add line through task title
+            updateRequest.onerror = function() {
+              console.error('Error updating task in IndexedDB');
+            };
+          }
+        });
+      };
+      getAllRequest.onerror=function(){
+        console.error('Error retrieving tasks from IndexedDB');
+      };
+  }
+  function notifyUser(taskName){
+    if (Notification.permission === 'granted') {
+      const notification = new Notification(`Task Due: ${taskName}`, {
+        body: 'This task is now due!',
+      });
+    }
+  }
+  // Request permission and check tasks periodically
+  if ('Notification' in window && Notification.permission !== 'denied') {
+    setInterval(checkAndNotifyTasks, 60000); // Check every minute
+  }
+  // Display tasks from IndexedDB on page load
+  getAllTasks();
 };
 
-// const app = (() => {
-//     'use strict';
-//     let swRegistration = null;
-
-//     const notifyButton = document.querySelector('.allow_nofication');
-
-//     // TODO 2.1 - check for notification support
-//     function displayNotification() {
-//       // TODO 2.2 - request permission to show notifications
-//     Notification.requestPermission(status=>{
-//         console.log('Notification permission ,',status)
-//       })
-//       // TODO 2.3 - display a Notification
-//       if(Notification.permission == 'granted'){
-//         navigator.serviceWorker.getRegistration().then(reg=>{
-//           const options={
-//             body:'any msg',
-//             icon:'../images/notification-flat.png',
-//             data:{
-//               dateOfArrival:Date.now(),
-//               primaryKey:1
-//             }}
-//           reg.showNotification('Hello World!',options)
-//         });
-//       }
-//     }
-//     notifyButton.addEventListener('click', () => {
-//       displayNotification();
-//     });
-
-//     if ('serviceWorker' in navigator) {
-//       window.addEventListener('load', () => {
-//         navigator.serviceWorker.register('sw.js')
-//         .then(swReg => {
-//           console.log('Service Worker is registered', swReg);
-//           swRegistration = swReg;
-//         })
-//         .catch(err => {
-//           console.error('Service Worker Error', err);
-//         });
-//       });
-//     } else {
-//       console.warn('Push messaging is not supported');
-//     }
-
-//   })();
+//handle allow notification button
+function displayNotification() {
+      Notification.requestPermission(status=>{
+          console.log('Notification permission',status)
+      });
+      notifyButton.classList.add('hidden')
+}
+notifyButton.addEventListener('click', () => {
+      displayNotification();
+});
